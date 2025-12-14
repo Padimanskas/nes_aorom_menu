50446efa8d7f4b34ec17e9736777b6b42778bf00
Stanislav Padimanskas <s.padimanskas@gmail.com>
2025-12-15 01:19:04 +0200

    init

---
+++ build.bat
@@ -0,0 +1,5 @@
+( cc65 -t nes -Oirs menu.c -o ./build/menu.s ) ^
+&& ( ca65 crt0.s -o ./build/crt0.o ) ^
+&& ( ca65 ./build/menu.s -o ./build/menu.o ) ^
+&& ( ld65 -C nes_aorom.cfg -o ./build/menu.nes ./build/crt0.o ./build/menu.o nes.lib ) ^
+&& ( "./tools/fceux-2.6.6-win64/fceux64.exe" ./build/menu.nes )
