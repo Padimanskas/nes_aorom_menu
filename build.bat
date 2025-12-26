f9d3165febde2298c5e4c2e905a4fbd98b31ae3d
Stanislav Padimanskas <s.padimanskas@gmail.com>
2025-12-27 01:00:53 +0200

    battletoads

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 ( cc65 -t nes -Oirs menu.c -o ./build/menu.s ) ^
 && ( ca65 crt0.s -o ./build/crt0.o ) ^
+&& ( ca65 chr.s -o ./build/chr.o ) ^
 && ( ca65 ./build/menu.s -o ./build/menu.o ) ^
-&& ( ld65 -C nes_aorom.cfg -o ./build/menu.nes ./build/crt0.o ./build/menu.o nes.lib ) ^
+&& ( ld65 -C nes_aorom.cfg -o ./build/menu.nes ./build/crt0.o ./build/chr.o ./build/menu.o nes.lib ) ^
 && ( "./tools/fceux-2.6.6-win64/fceux64.exe" ./build/menu.nes )
---
+++ chr.s
@@ -0,0 +1,6 @@
+.export _chr_data
+
+.segment "RODATA"
+
+_chr_data:
+    .incbin "chr.bin"
