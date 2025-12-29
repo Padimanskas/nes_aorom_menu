b2a30c7cef239b70b7a65db00b49a849af550d18
Stanislav Padimanskas <s.padimanskas@gmail.com>
2025-12-29 03:32:31 +0200

    delete unnecessary files after build

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@
 && ( ca65 chr.s -o ./build/chr.o ) ^
 && ( ca65 ./build/menu.s -o ./build/menu.o ) ^
 && ( ld65 -C nes_aorom.cfg -o ./build/menu.nes ./build/crt0.o ./build/chr.o ./build/menu.o nes.lib ) ^
+&& ( del /Q "build\crt0.o" "build\chr.o" "build\menu.o" "build\menu.s") ^
 && ( "./tools/fceux-2.6.6-win64/fceux64.exe" ./build/menu.nes )
